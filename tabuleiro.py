73910afcef092083179419a0ff7b1b61cdb67b36
Thiago Alves <77868152+tsalves@users.noreply.github.com>
2021-12-23 08:44:38 -0300

    Update tabuleiro.py

--- tabuleiro.py
+++ tabuleiro.py
@@ -43,7 +43,7 @@ class Tabuleiro:
     def iniciar(self, resultado = None):
 
         jogadores = 4
-        ativo = None
+        ativo = True
         for rodada in range(0, 999):
             if ativo == None:
                 break
